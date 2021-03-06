412c8cbbf0645cec5b2e8ffa1563d3fb48c44854
norr312 <norr312studio@gmail.com>
2021-03-06 23:42:33 +0200

    variables added to app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+//alert(1)
+//1 Variables
+var name = 'Alissa' // old version, can be changed
+const LastName = 'Serchenko' 
+let age = 38
+name = 'Olesya'
+console.log(name) //name will be shown on screen
