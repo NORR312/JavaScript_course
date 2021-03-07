1ed289ada25a3c2bc192f8a17624fbf6e89dfab2
norr312 <norr312studio@gmail.com>
2021-03-07 17:55:51 +0200

    operators priority added to app.js

--- app.js
+++ app.js
@@ -3,17 +3,17 @@
 //################################################################################################################################
 // style guid: valueName 
 // 99% case ;-symbole is not required
-var firstName = 'Alissa' // old version, can be changed, it's better don't use it
-const LastName = 'Serchenko' // readonly, can't be changed, but can be readed
+//var firstName = 'Alissa' // old version, can be changed, it's better don't use it
+//const LastName = 'Serchenko' // readonly, can't be changed, but can be readed
 //const age = 38
-const isProgrammer = true
+//const isProgrammer = true
 // if variable's name will not be changed, it's a good manner to use const
-let ageNow = 38
+//let ageNow = 38
 //name = 'Olesya'
 //console.log(firstName) //name will be shown on screen
 //console.log(LastName)
-ageNow = 'thirty eight' // data type can be changed
-//console.log(age)
+//ageNow = 'thirty eight' // data type can be changed
+////console.log(age)
 // _ , $, ... variables name accepted.
 // const 5Num = 5 - error, number can't be a start of variable name
 //console.log('Age of person:', age)
@@ -29,14 +29,52 @@ ageNow = 'thirty eight' // data type can be changed
 //const birthYear = 1982
 //const age = currentYear - birthYear
 //console.log(age)
-const a = 10
-const b = 5
-console.log(a+b)
-console.log(a-b)
-console.log(a*b)
-console.log(a/b)
+//const a = 10
+//const b = 5
+//let currentYear = 2021
+//let c = 32
+//console.log(a+b)
+//console.log(a-b)
+//console.log(a*b)
+//console.log(a/b)
+//console.log(currentYear++) //increment currentYear + 1: first console log -> increment, result: currentYear = 2021
+//console.log(currentYear) // already incremented currentYear = 2022
+//console.log(++currentYear) // first increment -> console log, result: currentYear = 2023
+//console.log(--currentYear) // first increment -> console log, result: currentYear = 2022
+//c+=a //c= c+a
+//c-=a //c= c-a
+//c*=a //c= c*a 
+//c/=a //c= c/a 
+//console.log(c)
 
-let currentYear = 2021
-console.log(currentYear++) //increment currentYear + 1: first console log -> increment, result: currentYear = 2021
-console.log(currentYear) // already incremented currentYear = 2022
-console.log(++currentYear) // first increment -> console log, result: currentYear = 2023
+//####################################################################################################3333
+//DATA TYPES
+//#####################################################################################################################
+//const isProgrammer = true
+//const name = 'Alissa'
+//const age = 38
+//let x
+//null
+//console.log(typeof isProgrammer) //result: boolean
+//console.log(typeof name) //result: string
+//console.log(typeof age) //result: number
+//console.log(typeof x) //result: undefined
+//console.log(typeof null) //result: object - a bug of javascript.
+// instead
+//console.log(null)
+
+//######################################################################
+// OPERATORS PRIORITY
+//###########################################################################################
+//const fullAge = 38
+//const birthYear = 1982
+//const currentYear = 2021
+//// > < >= <=
+//const isFullAge = currentYear - birthYear >= fullAge
+//console.log(isFullAge)
+
+//##############################################################################
+//CONDITIONAL OPERATORS
+//###############################################################################################
+const courseStatus = 'pending'
+
