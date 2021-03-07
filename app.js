0c78f3ec45a751d62a426fc94d742ef564ac1569
norr312 <norr312studio@gmail.com>
2021-03-07 12:33:34 +0200

    added alert function into app.js

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 //alert(1)
 //1 Variables
-var name = 'Alissa' // old version, can be changed
-const LastName = 'Serchenko' 
-let age = 38
-name = 'Olesya'
-console.log(name) //name will be shown on screen
+// style guid: valueName 
+// 99% case ;-symbole is not required
+var firstName = 'Alissa' // old version, can be changed, it's better don't use it
+const LastName = 'Serchenko' // readonly, can't be changed, but can be readed
+const age = 38
+const isProgrammer = true
+// if variable's name will not be changed, it's a good manner to use const
+let ageNow = 38
+//name = 'Olesya'
+console.log(firstName) //name will be shown on screen
+console.log(LastName)
+ageNow = 'thirty eight' // data type can be changed
+console.log(age)
+// _ , $, ... variables name accepted.
+// const 5Num = 5 - error, number can't be a start of variable name
+console.log('Age of person:', age)
+console.log('Name of person: ' + firstName + ', and age is '+age)
+alert('Name of person: ' + firstName + ', and age is '+age) //alert function in browser, not in JavaScript
+
