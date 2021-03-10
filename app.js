5310ac18ea5a468c62ad40dd851588b3b1314b49
norr312 <norr312studio@gmail.com>
2021-03-11 00:02:36 +0200

    app.js changed to 02_number.js, strings chapter started.

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
-//NUMBERS
-//const num = 42
-//const float = 42.42
-//const pow=10e3 // 10**3
-// Number.MAX_SAFE_INTEGER - max number
-//console.log('MAX_SAFE_INTEGER', Number.MAX_SAFE_INTEGER)
-//console.log('Math.pow', Math.pow(2, 53)-1)
-//console.log('MIN_SAFE_INTEGER', Number.MIN_SAFE_INTEGER)
-//console.log('MAX_VALUE', Number.MAX_VALUE)
-//console.log('MIN_VALUE', Number.MIN_VALUE)
-//console.log('POSITIVE_INFINITY', Number.POSITIVE_INFINITY)
-//console.log('NEGATIVE_INFINITY', Number.NEGATIVE_INFINITY)
-//console.log('NEGATIVE_INFINITY', 1/0, 2/0) // result: Infinity
-//console.log(Number.NaN, 'type of NaN:', typeof NaN)
-//const wierd = 2/undefined
-//console.log('2/undefined', wierd, 'isNaN(wierd):', isNaN(wierd), 'isNaN(2):',isNaN(2))
-//console.log('2/undefined', wierd, 'Number.isFinite(wierd):', Number.isFinite(wierd), 'Number.isFinite(2):', isFinite(2))
-////console.log('2/undefined', wierd, isNaN(wierd), isNaN(2))
-//const stringInt = '40'
-//const stringFloat ='40.42'
-//console.log(stringInt + 2)
-//console.log(Number.parseInt(stringInt) + 2) //string to integer number
-//console.log(+stringInt + 2) //string to integer number
-//console.log(parseFloat(stringFloat) + 0.5) //string to float number
-//console.log(+stringFloat + 0.5) //string to float number
+//STRING
+//const name='Alissa'
+//const age= 45
+//const output='My name is '+name+' and I am '+age+' y.o.'
+//console.log(output)
+//function getAge(){
+    //return age
+//}
+//const output = `Hi, my name is ${name} and my age is ${getAge()}.`//analog in python: 'string {arg}'.format(arg)
+//console.log(output)
+// acceptable 
+//const output = `Hi, my name is ${age >40 ? 'grandma' : 'Alissa'}  and my age is ${getAge()}.`
+//console.log(output)
+// `` keep all modifications of text
+//const output =`<div>This is div</div>
+//<p>this is O</p>`
+//console.log(output)
+const newName="Alissa"
+console.log(newName.length)
+console.log(newName.toUpperCase())
+console.log(newName.toLocaleLowerCase())
+console.log(newName.charAt(2))
+console.log(newName.indexOf('ss'))
+console.log(newName.indexOf("k"))
+//console.log(newName.toUpperCase())
+//console.log(newName.toUpperCase())
+//console.log(newName.toUpperCase())
